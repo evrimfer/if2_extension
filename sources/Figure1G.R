598e8f404f273c12345ba6ffc11a5c39335300fa
evrimfer <evrimfer@gmail.com>
2026-09-11 22:54:54 +0200

    Add files via upload

---
+++ sources/Figure1G.R
@@ -0,0 +1,50 @@
+# scripts for making Figure 2 Panel G
+# the source data table generated from the direct disorder results using script 07_preferences.R
+
+temperature_preferences=read.csv("/Users/evrimfer/Documents/KacarLab/publications/2026-Fer-Extensions/submission/publication-revisions/SourceData/SourceDataFig1G.csv")
+
+# number of samples
+sum(temperature_preferences$temperatureType=="psychrophile")
+sum(temperature_preferences$temperatureType=="mesophile")
+sum(temperature_preferences$temperatureType=="thermophile")
+
+# psychrophile n=4
+# mesophile n=707
+# thermophile n=84
+
+
+library(ggplot2)
+
+temperature_preferences$temperatureType=factor(temperature_preferences$temperatureType, levels=c("psychrophile","mesophile","thermophile"))
+
+
+ggplot(temperature_preferences, aes(x=temperatureType, y=as.numeric(L_IF2N), fill=source))+
+  geom_boxplot(width=0.5, alpha=0.6) +
+  geom_jitter(width=0, alpha=0.4)+
+  scale_fill_manual(values=c("#fdce9d"))+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),legend.position="none",
+        axis.text=element_text(size=12, face="bold", color="black"), axis.title = element_text(size=14, face="bold", color="black"),
+        plot.title = element_text(hjust = 0.5, size=16, face="bold")) +
+  labs(title=" ", x="", y="IF2 N-terminal Length")
+
+ggplot(temperature_preferences, aes(x=temperatureType, y=as.numeric(L_IF2C), fill=source))+
+  geom_boxplot(width=0.5, alpha=0.6) +
+  geom_jitter(width=0, alpha=0.4)+
+  scale_fill_manual(values=c("#fdce9d"))+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),legend.position="none",
+        axis.text=element_text(size=12, face="bold", color="black"), axis.title = element_text(size=14, face="bold", color="black"),
+        plot.title = element_text(hjust = 0.5, size=16, face="bold")) +
+  labs(title=" ", x="", y="IF2 C-terminal Length")
+
+# statistical test between preferences
+# NTD
+t.test(as.numeric(temperature_preferences[temperature_preferences$temperatureType=="mesophile",3]),as.numeric(temperature_preferences[temperature_preferences$temperatureType=="psychrophile",3]))
+t.test(as.numeric(temperature_preferences[temperature_preferences$temperatureType=="mesophile",3]),as.numeric(temperature_preferences[temperature_preferences$temperatureType=="thermophile",3]))
+t.test(as.numeric(temperature_preferences[temperature_preferences$temperatureType=="psychrophile",3]),as.numeric(temperature_preferences[temperature_preferences$temperatureType=="thermophile",3]))
+# CTD
+t.test(as.numeric(temperature_preferences[temperature_preferences$temperatureType=="mesophile",4]),as.numeric(temperature_preferences[temperature_preferences$temperatureType=="psychrophile",4]))
+t.test(as.numeric(temperature_preferences[temperature_preferences$temperatureType=="mesophile",4]),as.numeric(temperature_preferences[temperature_preferences$temperatureType=="thermophile",4]))
+t.test(as.numeric(temperature_preferences[temperature_preferences$temperatureType=="psychrophile",4]),as.numeric(temperature_preferences[temperature_preferences$temperatureType=="thermophile",4]))
+
